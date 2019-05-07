d9227ea8525810a37b78eef750929a27fe29d7f9
Ronnie Mathew <ronnie@bricksimple.com>
2019-05-07 10:33:02 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { DocumentComponent } from './document/document.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = {
+    url: 'http://localhost:4444',
+    options: {},
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DocumentListComponent,
+        DocumentComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        SocketIoModule.forRoot(config),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
